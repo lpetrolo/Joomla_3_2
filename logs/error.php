9ebb01612b72d92cece49a6180a350b9956797a0
lpetrolo <lucapetrolo@gmail.com>
2013-11-17 23:02:37 +0100

    upload

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-11-17 21:52:15 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-11-17T21:52:15+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-11-17T21:53:09+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
